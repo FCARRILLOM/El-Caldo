8318d4d23575fcc1047a17bcf6bd4ec6f802babe
PatricioSaldivar <patricio.saldivarf@gmail.com>
2019-08-24 16:31:48 -0500

    Itegrated Google Translate Api and made function for tanslating any text on any language

--- SpeechBubble/SpeechBubble/ViewController.swift
+++ SpeechBubble/SpeechBubble/ViewController.swift
@@ -11,6 +11,9 @@ import SceneKit
 import ARKit
 import AVFoundation
 import Speech
+import ROGoogleTranslate
+
+
 
 class ViewController: UIViewController, ARSCNViewDelegate {
     
@@ -26,7 +29,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -78,9 +80,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             (result, _) in
             if let transcription = result?.bestTranscription {
                 self.speechText = transcription.formattedString
-                if let texto = self.speechText{
-                    print(texto)
-                }
+                //if let texto = self.speechText{
+                  //  print(texto)
+                //}
+                //Translates the text
+                
             }
         }
     }
@@ -91,6 +95,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         recognitionTask?.cancel()
         audioEngine.inputNode.removeTap(onBus: 0)
         self.speechText = ""
+        
     }
 
     // MARK: - ARSCNViewDelegate
@@ -117,5 +122,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    
+    
+    func Translate(src: String,tgt: String,txt: String)->String?{
+       
+        
+        let params = ROGoogleTranslateParams(source: src,
+                                             target: tgt,
+                                             text:   txt)
+        var text : String?
+        let translator = ROGoogleTranslate()
+        
+        translator.translate(params: params) { (result) in
+             text = result;
+        }
+        return text
     }
 }
